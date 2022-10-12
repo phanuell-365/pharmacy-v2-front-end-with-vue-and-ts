af1b8190326aabd13f8e97c79088065ce7a5a7ae
phanuell-365 <phanuelkidakwa@gmail.com>
2022-10-12 13:06:34 +0300

    Changed the carousel icons

--- src/components/dashboard/item/CardTotalIcon.vue
+++ src/components/dashboard/item/CardTotalIcon.vue
@@ -14,7 +14,7 @@ const props = defineProps<CardTotalProps>();
 const icon = computed(() =>
   props.rate > 4
     ? "fa-arrow-up-wide-short text-warning"
-    : "fa-sort-amount-down text-danger"
+    : "fa-arrow-down-wide-short text-danger"
 );
 // <i class="fa-solid fa-arrow-trend-up"></i>;
 </script>
