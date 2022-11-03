ec871b5abeacffdc5762d32741f2b61a8582dce8
phanuell-365 <phanuelkidakwa@gmail.com>
2022-11-03 11:25:46 +0300

    Uncommented imports for resources in the main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,11 @@ import App from "./App.vue";
 import router from "./router";
 
 // import "./assets/main.css";
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import "@popperjs/core";
-// import "bootstrap/dist/js/bootstrap.min";
-// import "bootstrap";
-// import "@/assets/css/all.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@popperjs/core";
+import "bootstrap/dist/js/bootstrap.min";
+import "bootstrap";
+import "@/assets/css/all.css";
 
 const app = createApp(App);
 
